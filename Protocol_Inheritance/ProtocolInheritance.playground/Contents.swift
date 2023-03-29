f2db961813d1442a9a5f20f5f1e8a6c03399a57f
eimrahor <emirhan.imrahor@gmail.com>
2023-03-29 15:21:29 +0300

    Multiple Protocols

---
+++ Protocol_Inheritance/ProtocolInheritance.playground/Contents.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+protocol Taggable {
+    var tag: String { get }
+    var data: Data { get }
+    init(tag: String, data: Data)
+}
+
+protocol TaggedPersistable: Taggable, CustomStringConvertible, Equatable {
+    init(tag: String, contentsOf url: URL) throws
+    func persist(to url: URL) throws
+}
+
+protocol TaggedEncodable: Taggable {
+    var base64: String { get }
+}
+
+struct MyData: Taggable {
+    var tag: String
+    
+    var data: Data
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
+
+extension MyData: TaggedPersistable {
+    init(tag: String, contentsOf url: URL) throws {
+        let data = try Data.init(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+    
+    func persist(to url: URL) throws {
+        try self.data.write(to: url)
+    }
+}
+
+extension MyData: CustomStringConvertible {
+    var description: String {
+        return "My data(\(tag))"
+    }
+}
+
+extension MyData: TaggedEncodable {
+    var base64: String {
+        return self.data.base64EncodedString()
+    }
+}
+
