11d457c9a141186b2732f2b39141f0b5a0fe0866
eimrahor <emirhan.imrahor@gmail.com>
2023-03-31 17:29:49 +0300

    class and protocol

---
+++ Pillarse-of-POP-Challenge/Challenge.playground/Sources/Services.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+public protocol Service {
+    var total: Float { get }
+    mutating func add(payment: Float)
+}
+
+private class AmazonService {
+    private var balance: Float = 0
+    
+    fileprivate init() {}
+    
+    private func orderPlaced(payment: Float) {
+        balance += payment
+    }
+    private var earnings: Float {
+        return balance
+    }
+}
+
+extension AmazonService: Service {
+    var total: Float {
+        return balance
+    }
+    
+    func add(payment: Float) {
+        orderPlaced(payment: payment)
+    }
+}
+
+private class EtsyService {
+    private var earnings: Float = 0
+    
+    fileprivate init() {}
+    
+    private func itemSold(profit: Float) {
+        earnings += profit
+    }
+    private var totalSold: Float {
+        return earnings
+    }
+}
+
+extension EtsyService: Service {
+    var total: Float {
+        return earnings
+    }
+    
+    func add(payment: Float) {
+        return itemSold(profit: payment)
+    }
+}
+
+public enum ServiceType {
+    case amazon
+    case etsy
+}
+
+public func make(service: ServiceType) -> Service {
+    switch service {
+    case .amazon:
+        return AmazonService()
+    case .etsy:
+        return EtsyService()
+    }
+}
